970d8d98960cc689d98dade24cb125947fe8d198
WilliamSirois <157057080+WilliamSirois@users.noreply.github.com>
2025-03-31 14:37:34 -0400

    Update CodeOpta.ino
    We added comments and standardized the variable names.

--- CodeOpta.ino
+++ CodeOpta.ino
@@ -1,135 +1,156 @@
-#include <SPI.h>
-#include <Ethernet.h>   // Pour la connexion Ethernet
-#include <HttpClient.h> // Pour envoyer les données HTTP
-#include <ArduinoModbus.h>
-#include <ArduinoRS485.h>
+#include <Ethernet.h>     // Pour établir la connexion à l'aide d'Ethernet de l'arduino.
+#include <HttpClient.h>   // Pour envoyer les données HTTP de l'arduino vers le serveur.
+#include <ArduinoModbus.h>// Librairie qui permet d'utiliser le port Modbus de l'arduino avec le thermomètre humidité-temperature.
+#include <ArduinoRS485.h> // Librairie pour l'utilisation du standard RS-485 avec le themomètre humidité-temperature.
 
-float T0 = 50 + 273.15; // Température de référence (K)
-int RT0 = 988.1; // Résistance de référence à 50°C (ohms)
-int B = 4100; // Constante beta (K)
-int R = 2985; // Résistance en série avec le thermomètre (ohms)
-float division1;
-float division2;
-float lecture_I3;
-float lecture_I4;
+//Liste de constantes nécessaires aux calculs
+float TEMPERATURE_REFERENCE = 50 + 273.15; // Température de référence (K)
+int RESISTANCE_REFERENCE = 988.1; // Résistance de référence à 50°C (ohms)
+int BETA = 4100; // Constante beta (K)
+int RESISTANCE_SERIE = 2985; // Résistance en série avec le thermomètre (ohms)
 
+//Constantes pour la communication avec le Modbus par RS-485
+constexpr auto BAUDRATE = 9600; // Débit de comminucation établie à 9600 bits/seconde.
+constexpr auto BITDURATION = 1.f / BAUDRATE; // Durée d'un seul bit.
+constexpr auto WORD_LENGTH = 10.0f; // Établissement de la longueur d'un seul mot en nombre de bits.
+// Calcul du délai antérieur et postérieur en microsecondes pour une transmission RS-485 stable
+constexpr auto preDelayBR = BITDURATION * WORD_LENGTH * 3.5f * 1e6; // Ici il s'agit du calcul du pré-délai avant la transmission.
+constexpr auto postDelayBR = BITDURATION * WORD_LENGTH * 3.5f * 1e6 / 2.0; // Ici il s'agit du calcul du post-délai après la transmission.
 
-float temp1, temp2, tempIRM, humIRM;
+//Liste de variables utilisées dans les calculs de températures.
+float division_thermometre1, division2, lecture_entree, lecture_sortie;
+float temp_entree, temp_sortie, temp_irm, hum_irm;
 float voltage_thermistor1, voltage_thermistor2;
 float resistance_thermistor1, resistance_thermistor2;
 
-//Chemin sur lequel le pushgateway attend les données
+//Constantes requises au bon fonctionnement de l'envoi de données.
 String url ="/metrics/job/temperature";
+int serverPort = 9091;  // Port du Prometheus Pushgateway.
+byte mac[] = { 0xA8, 0x61, 0x0A, 0x50, 0x5A, 0xE7 }; // Adresse MAC assigné à l'arduino.
+IPAddress server(10, 200, 38, 184); // Adresse IP du serveur qui reçoit les données.
 
-int serverPort = 9091;  // Port du Pushgateway
-byte mac[] = { 0xA8, 0x61, 0x0A, 0x50, 0x5A, 0xE7 }; // Adresse MAC de l'Arduino
-IPAddress server(); // À remplacer par l'IP du serveur UNF
+//Création des objets pour la connexion.
+EthernetClient ethernetClient; // Création d'un objet de type EthernetClient pour établir une connexion réseau.
+HttpClient client(ethernetClient); // Création d'un objet nommé client pour envoyer des requêtes HTTP à partir de la conexion établie par ethernetClient.
 
-EthernetClient ethernetClient; // Client Ethernet
-HttpClient client(ethernetClient); // Client HTTP
-//On met l'adresse IP en chaine de caractères pour utiliser HttpClient plus tard
+//Fonctions utile tout le long du code. 
+// Fonction pour le calcul de la résistance selon la lecture d'une PINS utilisé pour les thermomètres de l'eau.
+float calcultemperature (float lecture_pin) {
+  float voltage= (5/1935.00) * lecture_pin; //Lecture  sur 5V
+  float resistance = (voltage * RESISTANCE_SERIE) / (5.0 - voltage); //Calcul de la résistance par diviseur de tension.
+  float division = log(resistance/RESISTANCE_REFERENCE); // calcul de division pour suivre la courbe du thermomètre.
+  float temp = (1 / ((division/BETA)+(1/TEMPERATURE_REFERENCE))) - 273.15; // Calcul de la température.
+  return temp;
+}
 
-constexpr auto baudrate = 9600;
-constexpr auto bitduration = 1.f / baudrate;
-constexpr auto wordlen = 10.0f;
-// Calcul du délai antérieur et postérieur en microsecondes pour une transmission RS485 stable
-constexpr auto preDelayBR = bitduration * wordlen * 3.5f * 1e6;
-constexpr auto postDelayBR = bitduration * wordlen * 3.5f * 1e6 / 2.0;
+//Fonction pour lire la température et l'humidité avec le Modbus.
+float ReadRS485(int addr, int reg) {
+  int w1 = 0;
+  if (!ModbusRTUClient.requestFrom(addr, HOLDING_REGISTERS, reg, 1)) {
+    Serial.println("Erreur lecture Modbus");
+    Serial.println(ModbusRTUClient.lastError());
+  } else {
+    w1 = ModbusRTUClient.read();
+  }
+  return w1;
+}
 
 void setup() {
-    //Initiation des entrées et des sorties 
-  pinMode(LED_D0, OUTPUT); //Sortie LED du Opta pour signal visuel qu'il est en fonctionnement
-  digitalWrite(LED_D0, HIGH);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(D2, OUTPUT);
+  //Initiation des PINS d'entrées et de sorties.
+  pinMode(LED_D0, OUTPUT); //Sortie LED directement intégrée au opta nous signal son bon fonctionnement.
+  digitalWrite(LED_D0, HIGH); //Nous allumons cette même LED.
+  pinMode(A2, INPUT); // PIN identifiée comme étant I3 sur l'Arduino, sert à receuillir les données de l'eau d'entrée. 
+  pinMode(A3, INPUT); // PIN identifiée comme étant I4 sur l'Arduino, sert à receuillir les données de l'eau de sortie.
+  // Manque les pins pour le débimètre.
 
-  Serial.begin(baudrate);
-  analogReadResolution(12);
+  Serial.begin(BAUDRATE); // On initialise la connexion par le port USB pour permettre la connexion à un ordinateur en cas de problème. 
+  analogReadResolution(12); // On augmente la résolution de lecture de l'arduino pour la mettre sur 12 bits.
 
   // Initialisation de l'Ethernet
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Échec de la configuration Ethernet avec DHCP");
-    while (true);  // Bloque le programme si l'Ethernet échoue
+  if (Ethernet.begin(mac) == 0) { // On lance la connexion et on vérifie si nous avons réussi à avoir une adresse IP de DHCP.
+    Serial.println("Échec de la configuration Ethernet avec DHCP"); //On imprimme un message d'erreur dans le serial print.
+    while (true);  // Bloque le programme si l'Ethernet échoue.
   }
 
-  delay(2000);
-  Serial.print("Connecté à Ethernet, adresse IP : ");
+  delay(2000); // Ajout d'un délai pour s'assurer que la connexion est bien établie.
+  Serial.print("Connecté à Ethernet, adresse IP : "); // On imprimme l'adresse IP dans le terminal de l'ordinateur (Une seule fois à l'initialisation).
   Serial.println(Ethernet.localIP());
 
+//Validation de la connection au serveur. On imprimme dans le terminal.
   if (!client.connect(server, serverPort)) {
     Serial.println(" Échec de connexion au serveur !");
 } else {
     Serial.println(" Connexion réussie !");
 }
 
-  // Initialisation de la communication RS485
+// Initialisation de la communication RS485 pour le Modbus donc le thermomètre humidité-température.
   RS485.setDelays(preDelayBR, postDelayBR);
-  if (!ModbusRTUClient.begin(baudrate, SERIAL_8N2)) {
+  if (!ModbusRTUClient.begin(BAUDRATE, SERIAL_8N2)) { // On indique le nombre de bits d'information (8), le nombre de bits de parité (0 donc N) et le nombre de bits de stop (2).
     Serial.println("Erreur Modbus");
     while (true);
   }
 }
 
 void loop() {
-
-  digitalWrite(D2, HIGH); // Active la sortie digitale
-  digitalWrite(LED_D2, HIGH); //Nous permet de voir si le code marche bien
+  digitalWrite(LED_D2, HIGH); //Nous permet de voir si le code entre bien dans la boucle en allumant une LED.
 
   //Lecture des thermistors
-  lecture_I4=analogRead(A3);
-  lecture_I3 =analogRead(A2);
-  voltage_thermistor1 = (5/1935.00) * lecture_I3; //Lecture 5V
-  voltage_thermistor2 = (5/1935.00) * lecture_I4; //Lecture 5V
+  lecture_sortie=analogRead(A3);
+  lecture_entree =analogRead(A2);
+  temp_entree = calcultemperature(lecture_entree);
+  temp_sortie = calcultemperature(lecture_sortie);
 
+/*  À valider si le changement est bon : 
+  voltage_thermistor1 = (5/1935.00) * lecture_entree; //Lecture 5V
+  voltage_thermistor2 = (5/1935.00) * lecture_sortie; //Lecture 5V
   // Calcul de la résistance des thermistors (loi d'Ohm)
-  resistance_thermistor1 = (voltage_thermistor1 * R) / (5.0 - voltage_thermistor1);
-  resistance_thermistor2 = (voltage_thermistor2 * R) / (5.0 - voltage_thermistor2);
+  resistance_thermistor1 = (voltage_thermistor1 * RESISTANCE_SERIE) / (5.0 - voltage_thermistor1);
+  resistance_thermistor2 = (voltage_thermistor2 * RESISTANCE_SERIE) / (5.0 - voltage_thermistor2);
 
   // Calcul de la température
-  division1 = log(resistance_thermistor1/RT0);
-  temp1 = (1 / ((division1/B)+(1/T0))) - 273.15;
-  division2 = log(resistance_thermistor2/RT0);
-  temp2 = (1 / ((division2/B)+(1/T0))) - 273.15;
+  division_thermometre1 = log(resistance_thermistor1/RESISTANCE_REFERENCE);
+  temp_entree = (1 / ((division_thermometre1/BETA)+(1/TEMPERATURE_REFERENCE))) - 273.15;
+  division2 = log(resistance_thermistor2/RESISTANCE_REFERENCE);
+  temp_sortie = (1 / ((division2/BETA)+(1/TEMPERATURE_REFERENCE))) - 273.15;
+*/
 
   //Lecture de la température et de l'humidité de la salle IRM 
   //Requête de données à l'adresse 0x01 (par défault) dans le registre 0x00 (température) et 0x01 (humidité)
-  tempIRM = ReadRS485(0x01, 0x00)/100;
-  humIRM = ReadRS485(0x01, 0x01)/100;
+  temp_irm = ReadRS485(0x01, 0x00)/100;
+  hum_irm = ReadRS485(0x01, 0x01)/100;
 
-  // Affichage des données
+  // Affichage des données dans le serial print afin de détecter les erreurs.
   Serial.print("Température Entrée (°C): ");
-  Serial.println(temp1);
+  Serial.println(temp_entree);
   Serial.print("Température Sortie (°C): ");
-  Serial.println(temp2);
+  Serial.println(temp_sortie);
   Serial.print("Température Salle IRM (°C): ");
-  Serial.println(tempIRM);
+  Serial.println(temp_irm);
   Serial.print("Humidité Salle IRM (%): ");
-  Serial.println(humIRM);
+  Serial.println(hum_irm);
 
   // Préparation des données à envoyer
   String postData = "# HELP temperature_input Temperature in Celsius at input\n";
   postData += "# TYPE temperature_input gauge\n";
-  postData += "temperature_input{label1=\"temp1\", location=\"entry\"} " + String(temp1);  // Température à l'entrée
+  postData += "temperature_input{label1=\"temp1\", location=\"entry\"} " + String(temp_entree);  // Température à l'entrée
   postData += "\n"; 
 
   postData += "# HELP temperature_output Temperature in Celsius at output\n";
   postData += "# TYPE temperature_output gauge\n";
-  postData += "temperature_output{label2=\"temp2\", location=\"exit\"} " + String(temp2);  // Température à la sortie
+  postData += "temperature_output{label2=\"temp2\", location=\"exit\"} " + String(temp_sortie);  // Température à la sortie
   postData += "\n";
 
   postData += "# HELP temperature_IRM Temperature IRM in Celsius\n";
   postData += "# TYPE temperature_IRM gauge\n";
-  postData += "temperature_IRM{label3=\"tempIRM\", unit=\"Celsius\"} " + String(tempIRM);  // Température IRM
+  postData += "temperature_IRM{label3=\"tempIRM\", unit=\"Celsius\"} " + String(temp_irm);  // Température IRM
   postData += "\n";
 
   postData += "# HELP humidity_IRM Humidity IRM in percentage\n";
   postData += "# TYPE humidity_IRM gauge\n";
-  postData += "humidity_IRM{label4=\"humIRM\", unit=\"percent\"} " + String(humIRM);  // Humidité IRM
+  postData += "humidity_IRM{label4=\"humIRM\", unit=\"percent\"} " + String(hum_irm);  // Humidité IRM
   postData += "\n";
 
+  // On convertie l'adresse IP pour l'obtenir en chaine de caractères.
   char serverStr[16];
   sprintf(serverStr, "%d.%d.%d.%d", server[0], server[1], server[2], server[3]);
 
@@ -142,7 +163,7 @@ void loop() {
   client.print(postData);
   client.endRequest();
 
-  // Lire la réponse du serveur
+  // Lire la réponse du serveur à chaque envoie de données
   int statusCode = client.responseStatusCode();
   Serial.print("Code réponse: ");
   Serial.println(statusCode);
@@ -152,13 +173,4 @@ void loop() {
   delay(5000); // Pause avant la prochaine envoi
 }
 
-float ReadRS485(int addr, int reg) {
-  int w1 = 0;
-  if (!ModbusRTUClient.requestFrom(addr, HOLDING_REGISTERS, reg, 1)) {
-    Serial.println("Erreur lecture Modbus");
-    Serial.println(ModbusRTUClient.lastError());
-  } else {
-    w1 = ModbusRTUClient.read();
-  }
-  return w1;
-}
+
